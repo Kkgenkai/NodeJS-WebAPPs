aac0740a021964d59c35ad8fe134381401cd83b8
kkgenkai <abenirecovery@gmail.com>
2024-09-22 20:22:05 +0300

    qr image piped directly fix

--- QR_Generator/server.js
+++ QR_Generator/server.js
@@ -4,26 +4,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
     const qr = require('qr-image');
-    const fs = require('fs');
-
-    let imgName = "qr_img.png";
 
     const url = "https://" + req.body.userInput;
-    let qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
-    res.sendFile(__dirname + "/" + imgName);
+    const qr_svg = qr.image(url, { type: 'png' });
+    
+    // Set the content type and send the image directly
+    res.set('Content-Type', 'image/png');
+    qr_svg.pipe(res);
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
